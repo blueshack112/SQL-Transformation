e05a35ce7585aa35445ab78747b3377e878f549d
Hassan Ahmed <ahmed.hassan.112.ha@gmail.com>
2020-03-09 09:33:17 +0500

    added -v and -h arguments

--- rearrange.py
+++ rearrange.py
@@ -4,6 +4,7 @@
 @author: Hassan Ahmed
 @contact: ahmed.hassan.112.ha@gmail.com
 @owner: Patrick Mahoney
+@version: 0.1.5
 
 This module is created to rearrange a CSV sheet expoerted from a SQL server.
 The CSV has three columns:
@@ -44,22 +45,14 @@ Usage:
     $ python rearrange.py -f [source.csv] -o [output.csv] -i 1000
     $ python rearrange.py -file [source.csv] --output_file [output.csv] --max_iterations 1000
 
-Attributes:
-    module_level_variable1 (int): Module level variables may be documented in
-        either the ``Attributes`` section of the module docstring, or in an
-        inline docstring immediately following the variable.
-
-        Either form is acceptable, but the two should not be mixed. Choose
-        one convention to document module level variables and be consistent
-        with it.
-
 Todo:
-    * Possibly add a -v or --verbose function where memory and time usage and other details are shown
+    * Add -v option's functionality
 
 Exit Statusses:
     - 2 -- Reading arguments
     - 3 -- Input file problems
     - 4 -- Output file problems
+    - 5 -- -h argument used
 """
 
 import sys, getopt
@@ -76,127 +69,57 @@ from  os import system
 ## Remove all debug variables
 current_milli_time = lambda: int(round(time.time() * 1000)) # For time measurement
 
+# Help message
+HELP_MESSAGE = """
+Usage:
+    From all the arguments available, the input file path is necessary and the
+    script will not work if it is not provided.
+    
+    $ python rearrange.py [-f <filepath>] [options]
+
+Parameters/Options:
+    -h                      : usage help and examples
+    -f  | --file            : define input CSV path 
+    -o  | --output_file     : define output CSV path
+    -i  | --max_iterations  : define max iterations allowed on a single GUID
+    -v  | --verbose         : show program execution details (may increase execution time)
+
+Example:
+    $ python rearrange.py -f [source.csv]
+    $ python rearrange.py -file [source.csv]
+
+    $ python rearrange.py -f [source.csv] -o [output.csv]
+    $ python rearrange.py -file [source.csv] --output_file [output.csv]
+
+    $ python rearrange.py -f [source.csv] -o [output.csv] -i 1000
+    $ python rearrange.py -file [source.csv] --output_file [output.csv] --max_iterations 1000
+"""
+
 def main (argv):
     # Parse arguments
-    inputFilePath, outputFilePath, maxItersPerGUID = parseArgs(argv)
-    print ("Initiating transformation...\nInput File              : {}\nOutput File             : {}\nMax Iterations per GUID : {}".format(inputFilePath, outputFilePath, maxItersPerGUID))
-    
-    # Create empty dataframe
-    outputcsv = pd.DataFrame(columns=['action','guid','ebayepid'])
+    inputFilePath, outputFilePath, maxItersPerGUID, VERBOSE = parseArgs(argv)
+    print ("""Initiating transformation...
+Input File              : {}
+Output File             : {}
+Max Iterations per GUID : {}
+Verbose                 : {}""".format(inputFilePath, outputFilePath, maxItersPerGUID, VERBOSE))
 
     # Read CSV and get it sorted
     csvfile = readAndSortCSV(inputFilePath)
-    numrows = csvfile.shape[0]
-
-    # Looping through all rows
-    # TODO: Modularize as much as possible from here    
-    # Functional variables
-    RUNNING_GUID = ''
-    RUNNING_EBAYEPID = ''
-    foundOnce = False
-    currentGUIDIters = 0
-    
-    # Time measurement variables
-    forStartTime = current_milli_time()
-    
-    # Progress publishing variables
-    stepDiv = int(numrows / 100)
-    steps = 0
-    progress = 0
-    progressPrefix = "Completed: {}%\t\t|\tIterations of current GUID: {}      "
 
     # Memory usage analysis start
     # tracemalloc.start()
 
-    print ("\nStarting main loop...")
-    print (progressPrefix.format(progress, currentGUIDIters), end='\r')    
-    for i in range (0, numrows):
-        steps = steps + 1
-        print (progressPrefix.format(progress, currentGUIDIters), end='\r')
-
-        if (steps >= stepDiv):
-            steps = 0
-            progress = progress + 1
-            if progress == 100:
-                print (progressPrefix.format(progress, currentGUIDIters))
-            else:
-                print (progressPrefix.format(progress, currentGUIDIters), end='\r')
-        
-        # Read one row
-        row = csvfile.iloc[i]
-        
-        # Extract columns from row
-        currentGUID = str(row[0])
-        currentEPID = str(row[1])
-        currentNote = str(row[2])
-
-        # if currentNote is NaN, convert it to NoneType
-        if currentNote == 'nan':
-            currentNote = None
-
-        # If the saved GUID is different than this row's GUID, then a new GUID has occured
-        if not RUNNING_GUID == currentGUID:
-            if foundOnce:
-                # Add current guid and ebayepid to the output csv
-                if len(RUNNING_EBAYEPID) > 0:
-                    RUNNING_EBAYEPID = RUNNING_EBAYEPID[:-1]
-                
-                tempdf = pd.DataFrame([['edit', RUNNING_GUID, RUNNING_EBAYEPID]], columns=['action','guid','ebayepid'])
-                outputcsv = outputcsv.append(tempdf, ignore_index=True)
-
-            RUNNING_GUID = currentGUID
-            RUNNING_EBAYEPID = ''
-            currentGUIDIters = 0
-
-            if not foundOnce:
-                foundOnce = True
-
-        # Check if iterations on current GUID have crossed the limit
-        if currentGUIDIters >= maxItersPerGUID:
-            continue
-        
-        # Concatenate current row's epid and note (if exists) to the RUNNING ebayepid
-        toconcat = currentEPID
-        if currentNote:
-            toconcat += "::{}*".format(currentNote)
-        else:
-            toconcat += "*"
-        RUNNING_EBAYEPID += toconcat
-        currentGUIDIters = currentGUIDIters + 1
-
-        # If the last row has arrived, add current guid and ebayepid to the output csv
-        if i == numrows-1:
-            if foundOnce:
-                # Add current guid and ebayepid to the output csv
-                if len(RUNNING_EBAYEPID) > 0:
-                    RUNNING_EBAYEPID = RUNNING_EBAYEPID[:-1]
-                
-                tempdf = pd.DataFrame([['edit', RUNNING_GUID, RUNNING_EBAYEPID]], columns=['action','guid','ebayepid'])
-                outputcsv = outputcsv.append(tempdf, ignore_index=True)
-        # Debug message:
-        """
-        system('cls')
-        print ("Completed:      {}%".format(progress))
-        print ("Saved GUID:     " + RUNNING_GUID)
-        print ("Saved EBAYEPID: " + RUNNING_EBAYEPID)
-        print ("Current GUID :  " + currentGUID)
-        print ("Current EPID :  " + currentEPID)
-        print ("Current Note :  " + str(currentNote))
-        print ("------------------------\n\n")
-        """
-    
-    # Calculate time taken
-    finalTime = current_milli_time() - forStartTime
-    print ("Main loop Complete.")
+    # Loop through file
+    outputcsv, finalTime = mainLoop(csvfile, maxItersPerGUID, VERBOSE)
 
     # Printing output to console and file
-    print ("\nPrinting output...")
-    print (outputcsv)
     outputcsv.to_csv(outputFilePath, index=False);
-
-    print ("\nOutput CSV has been written to: {}".format(outputFilePath))
-
-    print ("\nTime Taken by main loop: {} milliseconds".format(finalTime))
+    print ("\nPrinting output...")
+    
+    if VERBOSE: print (outputcsv, end='\n\n')
+    print ("Output CSV has been written to: {}".format(outputFilePath))
+    if VERBOSE: print ("\nTime Taken by main loop: {} milliseconds".format(finalTime))
 
     # Get memory usage peak
     # current, peak = tracemalloc.get_traced_memory()
@@ -290,24 +213,29 @@ def parseArgs (argv):
             Max iterations allowed per GUID after validations
     """
     # Defining options in for command line arguments
-    options = "hf:o:i:"
-    long_options = ["file=", "output_file=", "max_iterations="]
+    options = "hf:o:i:v"
+    long_options = ["file=", "output_file=", "max_iterations=", "verbose"]
     
     # Arguments
     inputFilePath = ''
     outputFilePath = ''
     maxItersPerGUID = 1000
+    VERBOSE = False
     
     # Extracting arguments
     try:
         opts, args = getopt.getopt(argv, options, long_options)
     except getopt.GetoptError:
-        print ("Error generated")
+        print ("Error in arguments!")
+        print (HELP_MESSAGE)
         sys.exit(2)
 
     for option, value in opts:
         if option == '-h':
-            print ("Asked for help")
+            print (HELP_MESSAGE)
+            sys.exit(5)
+        elif option in ("-v", "--verbose"):
+            VERBOSE = True
         elif option in ("-f", "--file"):
             inputFilePath = value
         elif option in ("-o", "--output_file"):
@@ -320,7 +248,131 @@ def parseArgs (argv):
 
     # Validate paths
     outputFilePath = validateFilePath(inputFilePath, outputFilePath)
-    return inputFilePath, outputFilePath, maxItersPerGUID
+    return inputFilePath, outputFilePath, maxItersPerGUID, VERBOSE
+
+def mainLoop(csvfile, maxItersPerGUID, VERBOSE):
+    """
+    The main loop that will iterate through every row and manipulate data
+    according to the arguments provided in the parameters.
+
+    Parameters
+    ----------
+        - csvfile : pd.dataframe
+            The dataframe that contains the contents of the input CSV file
+        - maxItersPerGUID : int
+            Maximum number of iteration allowed per GUID
+        - VERBOSE : bool
+            Whether to print looping details or not
+    
+    Returns
+    -------
+        - outputcsv : pd.dataframe
+            Dataframe that containst the final contents to be saved in the output CSV
+        - finalTime : long
+            Time taken by the whole loop
+    """
+    # Create empty dataframe
+    outputcsv = pd.DataFrame(columns=['action','guid','ebayepid'])
+
+    # Read number of lines
+    numrows = csvfile.shape[0]
+    
+    # Looping through all rows
+    # Functional variables
+    RUNNING_GUID = ''
+    RUNNING_EBAYEPID = ''
+    foundOnce = False
+    currentGUIDIters = 0
+    
+    # Time measurement variables
+    forStartTime = current_milli_time()
+    
+    # Progress publishing variables
+    stepDiv = int(numrows / 100)
+    steps = 0
+    progress = 0
+    progressPrefix = "Completed: {}%\t\t|\tIterations of current GUID: {}      "
+
+    print ("\nStarting main loop...")
+    for i in range (0, numrows):
+        if VERBOSE:
+            steps = steps + 1
+            if (steps >= stepDiv):
+                steps = 0
+                progress = progress + 1
+            
+            if progress < 100:
+                print (progressPrefix.format(progress, currentGUIDIters), end='\r')
+            elif progress == 100 and i == numrows-1:
+                print (progressPrefix.format(progress, currentGUIDIters))
+
+        # Read one row
+        row = csvfile.iloc[i]
+        
+        # Extract columns from row
+        currentGUID = str(row[0])
+        currentEPID = str(row[1])
+        currentNote = str(row[2])
+
+        # if currentNote is NaN, convert it to NoneType
+        if currentNote == 'nan':
+            currentNote = None
+
+        # If the saved GUID is different than this row's GUID, then a new GUID has occured
+        if not RUNNING_GUID == currentGUID:
+            if foundOnce:
+                # Add current guid and ebayepid to the output csv
+                if len(RUNNING_EBAYEPID) > 0:
+                    RUNNING_EBAYEPID = RUNNING_EBAYEPID[:-1]
+                
+                tempdf = pd.DataFrame([['edit', RUNNING_GUID, RUNNING_EBAYEPID]], columns=['action','guid','ebayepid'])
+                outputcsv = outputcsv.append(tempdf, ignore_index=True)
+
+            RUNNING_GUID = currentGUID
+            RUNNING_EBAYEPID = ''
+            currentGUIDIters = 0
+
+            if not foundOnce:
+                foundOnce = True
+
+        # Check if iterations on current GUID have crossed the limit
+        if currentGUIDIters >= maxItersPerGUID:
+            continue
+        
+        # Concatenate current row's epid and note (if exists) to the RUNNING ebayepid
+        toconcat = currentEPID
+        if currentNote:
+            toconcat += "::{}*".format(currentNote)
+        else:
+            toconcat += "*"
+        RUNNING_EBAYEPID += toconcat
+        currentGUIDIters = currentGUIDIters + 1
+
+        # If the last row has arrived, add current guid and ebayepid to the output csv
+        if i == numrows-1:
+            if foundOnce:
+                # Add current guid and ebayepid to the output csv
+                if len(RUNNING_EBAYEPID) > 0:
+                    RUNNING_EBAYEPID = RUNNING_EBAYEPID[:-1]
+                
+                tempdf = pd.DataFrame([['edit', RUNNING_GUID, RUNNING_EBAYEPID]], columns=['action','guid','ebayepid'])
+                outputcsv = outputcsv.append(tempdf, ignore_index=True)
+        # Debug message:
+        """
+        system('cls')
+        print ("Completed:      {}%".format(progress))
+        print ("Saved GUID:     " + RUNNING_GUID)
+        print ("Saved EBAYEPID: " + RUNNING_EBAYEPID)
+        print ("Current GUID :  " + currentGUID)
+        print ("Current EPID :  " + currentEPID)
+        print ("Current Note :  " + str(currentNote))
+        print ("------------------------\n\n")
+        """
+    
+    # Calculate time taken
+    finalTime = current_milli_time() - forStartTime
+    print ("Main loop Complete.")
+    return outputcsv, finalTime
 
 # Running script from command line
 if __name__ == "__main__":
